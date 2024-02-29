370668fb15002ca6d00c1b9ddcd8508ee8915cae
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2024-02-29 11:26:29 +0100

    update skhdrc; yabairc add borders; nix config.nix

--- cli/.config/nixpkgs/config.nix
+++ cli/.config/nixpkgs/config.nix
@@ -1,1 +1,8 @@
-{ allowUnfree = true; }
+{ allowBroken = true;
+  allowUnfree = true;
+  packageOverrides = pkgs:
+    {
+      python3 = pkgs.python3.overrideAttrs (attrs:
+        pkgs.lib.attrsets.recursiveUpdate attrs { meta.priority = -100; });
+    };
+}
