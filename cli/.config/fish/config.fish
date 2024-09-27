d4f1f988422e300bc9fc0b61213b4525e0b765ff
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2024-09-27 18:19:14 +0000

    make shell extension thf*ck optional

--- cli/.config/fish/config.fish
+++ cli/.config/fish/config.fish
@@ -2,4 +2,7 @@ set -x PATH $PATH ~/bin
 set -x PATH $PATH ~/.cargo/bin
 set -U TERMINAL kitty
 set -U EDITOR nvim
-thefuck --alias | source
+
+if type thefuck > /dev/null 2>&1
+    thefuck --alias | source
+end
