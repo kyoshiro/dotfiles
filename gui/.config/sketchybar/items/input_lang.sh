c359668debbc5408a6b5e1ce089cb380bd9d0871
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2025-01-19 16:06:44 +0100

    updated configs & vscode.extensions

--- gui/.config/sketchybar/items/input_lang.sh
+++ gui/.config/sketchybar/items/input_lang.sh
@@ -2,9 +2,10 @@
 
 sketchybar --add item input_source right
 sketchybar --set input_source \
-	icon.color=$LANG_COLOR_FG \
-	background.color=$LANG_COLOR_BG \
-        icon.font="$FONT:Regular:20.0" \
-	label.color=$LANG_COLOR_FG \
-    script="$PLUGIN_DIR/input_lang.sh" \
-    update_freq=1
+	         icon.color=$LANG_COLOR_FG \
+	         background.color=$LANG_COLOR_BG \
+                 icon.font="$FONT:Regular:20.0" \
+	         label.color=$LANG_COLOR_FG \
+                 script="$PLUGIN_DIR/input_lang.sh" \
+                 update_freq=1 \
+           --subscribe input_source mouse.clicked
