91a972e787ab82186309a0de991e00a78a626039
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2025-03-07 09:16:50 +0100

    update configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,7 +17,6 @@ require('packer').startup(function(use)
   use 'kyoshiro/ctrlp.vim'
   use 'kyoshiro/editorconfig-vim'
   use 'kyoshiro/fzf.vim'
-  use {'kyoshiro/nvim-treesitter',  run = ':TSUpdate'}
   use 'kyoshiro/ranger.vim'
   use 'kyoshiro/syntastic'
   use 'kyoshiro/tagbar'
@@ -56,7 +55,6 @@ require('packer').startup(function(use)
   use { 'kyoshiro/gitsigns.nvim', requires = { 'kyoshiro/plenary.nvim' } }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'kyoshiro/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'kyoshiro/cellular-automaton.nvim'
   use 'kyoshiro/nvim-tree.lua'
 end)
 
@@ -436,66 +434,6 @@ vim.keymap.set('n', '<leader>rc', ':set operatorfunc=RangerChangeOperator<cr>g@'
 vim.keymap.set('n', '<leader>rd', ':RangerCD<cr>')
 vim.keymap.set('n', '<leader>rld', ':RangerLCD<cr>')
 
-
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
--- require'nvim-treesitter'.setup {
---   -- A list of parser names or tiers ('stable', 'core', 'community', 'unsupported')
---   ensure_install = { "c", "lua", "vim", "vimdoc", "query"},
---
---   -- List of parsers to ignore installing
---   ignore_install = { 'unsupported' },
---
---   -- Automatically install missing parsers when entering buffer
---   auto_install = false,
---
---   -- Directory to install parsers and queries to
---   install_dir = vim.fn.stdpath('data') .. '/site',
---
---   highlight = {
---     enable = true,
---   }
--- }
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
