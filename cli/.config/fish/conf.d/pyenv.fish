e8268acf4b94b2b72954d2d51e3311330bb082d8
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2025-10-28 09:23:13 +0100

    add pyenv activate; terraform fmt in nvim; reset fastfetch

---
+++ cli/.config/fish/conf.d/pyenv.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+# Activate the pyenv virtual environment for Fish shell
+#
+if test -z ~/pyenv/bin/activate.fish
+    echo "pyenv virtual environment not found. Setting up pyenv first."
+    python3 -m venv ~/pyenv
+end
+
+source ~/pyenv/bin/activate.fish
--- nvim/.config/nvim/lua/plugins/terraform.lua
+++ nvim/.config/nvim/lua/plugins/terraform.lua
@@ -1,3 +1,7 @@
 return {
     "kyoshiro/vim-terraform",
+    ft = { "terraform", "tf" },
+    config = function()
+	vim.cmd([[autocmd BufWritePre *.tf,*.tfvars :TerraformFmt]])
+    end,
 }
